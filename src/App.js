449cc0fb4aaffe2abc3afb4372eb12816606a026
eniola-ajala <73951563+eniola-ajala@users.noreply.github.com>
2020-11-30 12:38:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter,Switch, Route } from 'react-router-dom'
 import Header from './component/header';
 // import CustomNav from './component/sidenav'
-// import Landing from './component/landingPage'
+// import Landing from './component/landinggPage'
 
 
 
