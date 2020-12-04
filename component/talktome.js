7f41f698e2702731444952b5a7e58bb13c9558ef
eniola-ajala <73951563+eniola-ajala@users.noreply.github.com>
2020-12-04 12:29:09 -0800

    first commit

---
+++ component/talktome.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Modal, Button, TextInput} from 'react-materialize'
+
+export default function Talk() {
+    return (
+        <div>
+            <Modal
+        actions={[
+          <Button flat modal="close" node="button" waves="green">
+            Close
+          </Button>,
+        ]}
+        bottomSheet={false}
+        fixedFooter={false}
+        header="Hire Me"
+        id="Modal-0"
+        open={false}
+        options={{
+          dismissible: true,
+          endingTop: "10%",
+          inDuration: 250,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          opacity: 0.5,
+          outDuration: 250,
+          preventScrolling: true,
+          startingTop: "4%",
+        }}
+          // root={[object HTMLBodyElement]}
+        trigger={<Button node="button">Hire Me</Button>}
+      >
+        
+        <form>
+          <TextInput id="TextInput-4" label="Name" />
+          <TextInput email id="TextInput-4" label="Email" validate />
+          <TextInput id="TextInput-12" label="Talk to me" />
+          <Button node="button" type="submit" waves="light">
+            {" "}
+            Submit{" "}
+          </Button>
+        </form>
+      </Modal>
+    </div>
+        
+    )
+}
---
+++ component/testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Testimonial() {
+    return (
+        <div className="testi">
+            <div>
+            <h3> Testimonials</h3>
+            </div>
+            
+        </div>
+    )
+}
